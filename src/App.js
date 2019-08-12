4123afbe21c9cc6beea96fc13aa94b0fd4c9e9da
Seshumadhav Chaturvedula <seshu@indeed.com>
2019-08-12 14:11:03 +0530

    Revert "Use React hooks instead of Class-components"
    This reverts commit fb92da7ab011f0f71589de19bdae70a33726ecdb.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,46 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import PersonComponent from './Person/Person'
 
-const app = props => {
-
-  const [personState, setPersonState] = useState({
+class App extends Component {
+  state = {
     persons: [
-      {name: 'Mr.Foo', age: 2},
-      {name: 'Mrs.Bar', age: 3},
-      {name: 'Ms.Bazz', age: 4}
+        {name: 'Mr.Foo', age: 2},
+        {name: 'Mrs.Bar', age: 3},
+        {name: 'Ms.Bazz', age: 4}
     ],
     animals: [
       {name: "Cat", age: 5}
     ]
-  });
+  }
 
-  const [birdState, setBirdState] = useState("This state is for birds");
-  console.log("Person State: "); console.log(personState); console.log("Bird state: "); console.log(birdState);
-
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     // console.log("Was clicked!");
-    // DON'T DO THIS: personState.persons[0].name = "Mr.Foo Fabulla";
-    setPersonState({
-      persons: [
+    // DON'T DO THIS: this.state.persons[0].name = "Mr.Foo Fabulla";
+    this.setState(
+      {persons: [
         {name: 'Mr.Foo Fabella', age: 22},
         {name: 'Mrs.Bar Buzak', age: 33},
         {name: 'Ms.Bazz Bhat', age: 44}
-      ],
-      // Uncomment if you want to change animals also
-      animals: [
-         {name: "Dog Vendetta", age: 55}
       ]
     });
+    this.state.persons[0].age = 22;
+  }
 
-    setBirdState("This is a new bird state");
-  };
-
-  return (
+  render() {
+    return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
         <p> This is really working!</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <PersonComponent name={personState.persons[0].name} age={personState.persons[0].age}/>
-        <PersonComponent name={personState.persons[1].name} age={personState.persons[1].age}>A Veteran</PersonComponent>
-        <PersonComponent name={personState.persons[2].name} age={personState.persons[2].age}>A Player</PersonComponent>
-        <PersonComponent name={personState.animals[0].name} age={personState.animals[0].age}>A mighty little cat</PersonComponent>
-        <PersonComponent name={birdState} age="0">Has some cute wings</PersonComponent>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <PersonComponent name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <PersonComponent name={this.state.persons[1].name} age={this.state.persons[1].age}>A Veteran</PersonComponent>
+        <PersonComponent name={this.state.persons[2].name} age={this.state.persons[2].age}>A Player</PersonComponent>
+        <PersonComponent name={this.state.animals[0].name} age={this.state.animals[0].age}>Mighty little one</PersonComponent>
       </div>
     );
- };
+  }
+}
 
-export default app;
+export default App;
